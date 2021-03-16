b97dd2731abf043fe4da296d17385b7061b0f0ba
FarukC5 <faruk_cajic@yahoo.com>
2021-03-16 09:54:11 +0100

    added

---
+++ ex09/src/index.js
@@ -0,0 +1,18 @@
+// Only Change below this line
+function isLess(a, b) {
+  // Only change above this line
+  if (a < b) {
+    // Only Change below this line
+    return true;
+    // Only change above this line
+  } else {
+    // Only Change below this line
+    return false;
+    // Only change above this line
+  }
+  // Only Change below this line
+}
+console.log(isLess(10, 15));
+console.log(isLess(15, 10));
+// Only change above this line
+module.exports = isLess;
